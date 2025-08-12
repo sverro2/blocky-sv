ec3bce2a5d9a5c8152511710680e620b149b5c50
Sven Brettschneider <svenbrettschneider@proton.me>
2025-08-12 08:57:38 +0200

    tweaking login

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,9 +1,13 @@
 import { db } from '$lib/server/db';
+import { requireAuth } from '$lib/server/auth';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async (event) => {
+	const user = requireAuth(event);
+
 	const projects = await db.query.project.findMany();
 	return {
-		projects
+		projects,
+		user
 	};
 };
